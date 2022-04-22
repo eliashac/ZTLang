c7311a4a4365e14b23b45417a6374209acc532ca
eliashac <elias.hachichou@gmail.com>
2022-04-22 12:40:32 +0200

    Added TestAccessPolicies

--- src/test/java/com/ztlang/test/TestAccessPolicies.java
+++ src/test/java/com/ztlang/test/TestAccessPolicies.java
@@ -19,60 +19,68 @@ import core.Attacker;
 import org.junit.jupiter.api.Test;
 
 public class TestAccessPolicies extends ZTLangTest {
-  private static class ZTLangModel {
-    public final ControlPlane controlplane = new ControlPlane("control plane");
-    
-    public final User alice = new User("alice");
-    public final User bob   = new User("bob");
-    public final EnterpriseResource resource = new EnterpriseResource("resource");
-    public final Device alice_device = new Device("alice device", false);
-    public final Device bob_device   = new Device("bob device", false);
-    public final UserCredentials alice_credentials = new UserCredentials("alice_credentials");
-    public final UserCredentials bob_credentials   = new UserCredentials("bob_credentials");
-    public final Agent alice_agent = new Agent("alice agent");
-    public final Agent bob_agent = new Agent("bob agent");
-
-    public ZTLangModel() {
-      controlplane.addAgent(alice_agent);
-      controlplane.addAgent(bob_agent);
-      controlplane.addResources(resource);
-      
-      alice.addDevices(alice_device);
-      alice.addUserCredentials(alice_credentials);
-      alice_agent.addDevice(alice_device);
-      alice_agent.addUser(alice);
-      alice.addResources(resource);
-
-      bob.addDevices(bob_device);
-      bob.addUserCredentials(bob_credentials);
-      bob_agent.addDevice(bob_device);
-      bob_agent.addUser(bob);
-    }
-  }
 
   @Test
   public void AccessResourceWithoutAccessPolicies() {
     // Bobs device is untrusted and he tries to access a resource with his credentials
-    var model = new ZTLangModel();
+    
+    // Adding assets
+    ControlPlane controlplane = new ControlPlane("control plane");
+    User bob   = new User("bob");
+    EnterpriseResource resource = new EnterpriseResource("resource");
+    Device bob_device   = new Device("bob device", false);
+    UserCredentials bob_credentials   = new UserCredentials("bob_credentials");
+    Agent bob_agent = new Agent("bob agent");
+    
+    // Setting up associations
+    controlplane.addAgent(bob_agent);
+    controlplane.addResources(resource);
+    bob.addDevices(bob_device);
+    bob.addUserCredentials(bob_credentials);
+    bob_agent.addDevice(bob_device);
+    bob_agent.addUser(bob);
+
+    // Attacker
     var attacker = new Attacker();
 
-    attacker.addAttackPoint(model.bob_credentials.Compromise);
-    attacker.addAttackPoint(model.bob_device.Compromise);
+    // Attack steps
+    attacker.addAttackPoint(bob_credentials.Compromise);
+    attacker.addAttackPoint(bob_device.Compromise);
+
     attacker.attack();
 
-    model.resource.Access.assertUncompromised();
+    resource.Access.assertUncompromised();
   }
 
   @Test
   public void AccessResourceWithAccessPolicies() {
     // Alices device is trusted and he tries to access a resource with her credentials
-    var model = new ZTLangModel();
+    
+    // Adding assets
+    ControlPlane controlplane = new ControlPlane("control plane");
+    User alice = new User("alice");
+    EnterpriseResource resource = new EnterpriseResource("resource");
+    Device alice_device = new Device("alice device", false);
+    UserCredentials alice_credentials = new UserCredentials("alice_credentials");
+    Agent alice_agent = new Agent("alice agent");
+    
+    // Setting up associations
+    controlplane.addAgent(alice_agent);
+    controlplane.addResources(resource);
+    alice.addDevices(alice_device);
+    alice.addUserCredentials(alice_credentials);
+    alice_agent.addDevice(alice_device);
+    alice_agent.addUser(alice);
+    alice.addResources(resource);
+
+    // Attacker
     var attacker = new Attacker();
 
-    attacker.addAttackPoint(model.alice_credentials.Compromise);
-    attacker.addAttackPoint(model.alice_device.Compromise);
+    // Attack steps
+    attacker.addAttackPoint(alice_credentials.Compromise);
+    attacker.addAttackPoint(alice_device.Compromise);
     attacker.attack();
 
-    model.resource.Access.assertCompromisedInstantaneously();
+    resource.Access.assertCompromisedInstantaneously();
   }
 }
