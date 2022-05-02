1b76d72362600334b38c7209826320d4e8d09271
eliashac <elias.hachichou@gmail.com>
2022-05-02 14:38:34 +0200

    Fixed use of model in some tests

--- src/test/java/com/ztlang/test/TestAccessPolicies.java
+++ src/test/java/com/ztlang/test/TestAccessPolicies.java
@@ -177,29 +177,82 @@ public class TestAccessPolicies extends ZTLangTest {
     resource.Access.assertUncompromised();
   }
 
+  // @Test
+  // public void testCompromiseAccessPoliciesAndPhishing() {
+  //   // Adding assets
+  //   ControlPlane controlplane = new ControlPlane("control plane");
+
+  //   EnterpriseResource resource = new EnterpriseResource("resource");
+  //   EnterpriseResource accessPolicies = new EnterpriseResource("accessPolicies");
+
+  //   User alice = new User("alice");
+  //   Device alice_device = new Device("alice device", false);
+  //   UserCredentials alice_credentials = new UserCredentials("alice_credentials");
+  //   Agent alice_agent = new Agent("alice agent");
+  //   AccessPolicy alice_accesspolicy = new AccessPolicy("Alice acesspolicy");
+
+  //   User bob = new User("bob");
+  //   Device bob_device   = new Device("bob device", false);
+  //   UserCredentials bob_credentials   = new UserCredentials("bob_credentials");
+  //   Agent bob_agent = new Agent("bob agent");
+  //   AccessPolicy bob_accesspolicy = new AccessPolicy("Bob acesspolicy");
+
+  //   // Setting up associations
+  //   controlplane.addAgent(bob_agent);
+  //   controlplane.addAgent(alice_agent);
+  //   controlplane.addResources(resource);
+  //   controlplane.addResources(accessPolicies);
+
+  //   alice.addDevices(alice_device);
+  //   alice.addUserCredentials(alice_credentials);
+  //   alice_agent.addDevice(alice_device);
+  //   alice_agent.addUser(alice);
+  //   alice_accesspolicy.addResource(resource);
+  //   alice.addAccessPolicy(alice_accesspolicy);
+
+  //   bob.addDevices(bob_device);
+  //   bob.addUserCredentials(bob_credentials);
+  //   bob_agent.addDevice(bob_device);
+  //   bob_agent.addUser(bob);
+  //   bob_accesspolicy.addResource(accessPolicies);
+  //   bob.addAccessPolicy(bob_accesspolicy);
+
+  //   accessPolicies.addAccessPolicy(alice_accesspolicy);
+  //   accessPolicies.addAccessPolicy(bob_accesspolicy);
+
+  //   var attacker = new Attacker();
+
+  //   attacker.addAttackPoint(bob_credentials.Compromise);
+  //   attacker.addAttackPoint(bob_device.Compromise);
+
+  //   attacker.addAttackPoint(alice_device.Compromise);
+  //   attacker.attack();
+
+  //   resource.Access.assertCompromisedInstantaneously();
+  // }
+
+
   @Test
   public void testCompromiseAccessPoliciesAndPhishing() {
     // Adding assets
     ControlPlane controlplane = new ControlPlane("control plane");
 
-    EnterpriseResource resource = new EnterpriseResource("resource");
-    EnterpriseResource accessPolicies = new EnterpriseResource("accessPolicies");
-
     User alice = new User("alice");
+    User charlie   = new User("charlie");
+    EnterpriseResource resource = new EnterpriseResource("resource");
+    AccessPolicies accessPolicies = new AccessPolicies("access policies");
     Device alice_device = new Device("alice device", false);
+    Device charlie_device = new Device("charlie device", false);
     UserCredentials alice_credentials = new UserCredentials("alice_credentials");
+    UserCredentials charlie_credentials   = new UserCredentials("charlie_credentials");
     Agent alice_agent = new Agent("alice agent");
+    Agent charlie_agent = new Agent("charlie agent");
+
     AccessPolicy alice_accesspolicy = new AccessPolicy("Alice acesspolicy");
+    AccessPolicy charlie_accesspolicy = new AccessPolicy("Charlie acesspolicy");
 
-    User bob = new User("bob");
-    Device bob_device   = new Device("bob device", false);
-    UserCredentials bob_credentials   = new UserCredentials("bob_credentials");
-    Agent bob_agent = new Agent("bob agent");
-    AccessPolicy bob_accesspolicy = new AccessPolicy("Bob acesspolicy");
-
-    // Setting up associations
-    controlplane.addAgent(bob_agent);
     controlplane.addAgent(alice_agent);
+    controlplane.addAgent(charlie_agent);
     controlplane.addResources(resource);
     controlplane.addResources(accessPolicies);
 
@@ -207,23 +260,29 @@ public class TestAccessPolicies extends ZTLangTest {
     alice.addUserCredentials(alice_credentials);
     alice_agent.addDevice(alice_device);
     alice_agent.addUser(alice);
-    alice_accesspolicy.addResource(resource);
-    alice.addAccessPolicy(alice_accesspolicy);
 
-    bob.addDevices(bob_device);
-    bob.addUserCredentials(bob_credentials);
-    bob_agent.addDevice(bob_device);
-    bob_agent.addUser(bob);
-    bob_accesspolicy.addResource(accessPolicies);
-    bob.addAccessPolicy(bob_accesspolicy);
+    charlie.addDevices(charlie_device);
+    charlie.addUserCredentials(charlie_credentials);
+    charlie_agent.addDevice(charlie_device);
+    charlie_agent.addUser(charlie);
+
+    alice.addAccessPolicy(alice_accesspolicy);
+    charlie.addAccessPolicy(charlie_accesspolicy);
+
+    alice_accesspolicy.addUser(alice);
+
+    alice_accesspolicy.addResource(resource);
+    charlie_accesspolicy.addResource(accessPolicies);
 
     accessPolicies.addAccessPolicy(alice_accesspolicy);
-    accessPolicies.addAccessPolicy(bob_accesspolicy);
+    accessPolicies.addAccessPolicy(charlie_accesspolicy);
+
+    controlplane.addStoredPolicy(alice_accesspolicy);
 
     var attacker = new Attacker();
 
-    attacker.addAttackPoint(bob_credentials.Compromise);
-    attacker.addAttackPoint(bob_device.Compromise);
+    attacker.addAttackPoint(charlie_credentials.Compromise);
+    attacker.addAttackPoint(charlie_device.Compromise);
 
     attacker.addAttackPoint(alice_device.Compromise);
     attacker.attack();
@@ -232,3 +291,6 @@ public class TestAccessPolicies extends ZTLangTest {
   }
 
 }
+
+
+
